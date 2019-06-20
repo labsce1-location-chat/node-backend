dd1091c89170292091ecee6dbd9483c89263f583
Drew <drewsup123c@gmail.com>
2019-06-20 13:41:37 -0600

    finished basic post request

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const server = express();
+const cors = require("cors");
+server.use(express.json());
+server.use(cors());
+
+const cloudinary = require('cloudinary');
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+server.post('/upload', (req, res) => {
+  // Upload image
+    cloudinary.v2.uploader.upload(req.body.img, function(
+    error,
+    result
+    ) {
+    if (error) {
+        return res.status(500).send(error);
+    }
+    // Send image Url
+    console.log(result)
+    res.status(200).send({url : result.url})
+    });
+});
+
+module.exports = server;
