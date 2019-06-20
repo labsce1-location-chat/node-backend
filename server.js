9f792b5b0bb8ccb85cc22c79084fe142148b51fe
Drew <drewsup123c@gmail.com>
2019-06-20 13:49:57 -0600

    done

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ server.use(express.json());
 server.use(cors());
 
 const cloudinary = require('cloudinary');
+// Setting up all necessary configuration for cloudinary
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
@@ -12,7 +13,7 @@ cloudinary.config({
 });
 
 server.post('/upload', (req, res) => {
-  // Upload image
+  // Upload image, Needs to be in base 64 format
     cloudinary.v2.uploader.upload(`data:image/jpg;base64${req.body.image}`, function(
     error,
     result
@@ -21,8 +22,12 @@ server.post('/upload', (req, res) => {
         return res.status(500).send(error);
     }
     // Send image Url
-    console.log(result)
+    // console.log(result)
     res.status(200).send({url : result.url})
+    // This will send a default object back with the url
+    // {
+    //     url : http://etc.com/fkjsalkfjf.jpg
+    // }
     });
 });
 
